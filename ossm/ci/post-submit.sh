cf68ecbf7290e6303a28a7d30781a7dd712dfce6
Zuzana Miklankova <zmiklank@redhat.com>
2025-11-24 14:48:54 +0100

    ossm: add script for uploading built ztunnel to gs
    Signed-off-by: Zuzana Miklankova <zmiklank@redhat.com>

---
+++ ossm/ci/post-submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -exo pipefail
+
+DIR=$(cd "$(dirname "$0")" ; pwd -P)
+
+GCS_PROJECT=${GCS_PROJECT:-maistra-prow-testing}
+ARTIFACTS_GCS_PATH=${ARTIFACTS_GCS_PATH:-gs://maistra-prow-testing/ztunnel}
+
+gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
+gcloud config set project "${GCS_PROJECT}"
+
+# Copy artifacts to GCS
+SHA="$(git rev-parse --verify HEAD)"
+
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  ARCH_SUFFIX="-arm64"
+else
+  ARCH_SUFFIX=""
+fi
+
+tar czf ./out/rust/release/ztunnel.tar.gz ./out/rust/release/ztunnel
+gsutil cp bazel-bin/envoy_tar.tar.gz "${ARTIFACTS_GCS_PATH}/ztunnel-alpha-${SHA}${ARCH_SUFFIX}.tar.gz"
